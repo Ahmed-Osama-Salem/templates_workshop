65023349a09bb82766af863c70661817267b667a
Ahmed-Osama-Salem <ahmedosamaweb@gmail.com>
2023-04-23 21:40:57 +0200

    pref(scrollCard):  adjust card component by list

--- src/component/sections/TemplateAbout.tsx
+++ src/component/sections/TemplateAbout.tsx
@@ -1,3 +1,5 @@
+import { scrollCardDB } from '@/source/db/scrollCard';
+
 import ContantCardScroll from '../modules/cards/ContantCardScroll';
 import CatchyHeader from '../modules/headers/CatchyHeader';
 
@@ -6,26 +8,15 @@ const TemplateAbout = () => {
     <div className="flex h-full w-full flex-col gap-[80px] px-[100px]">
       <CatchyHeader />
       <div className="flex flex-col gap-[140px]">
-        <ContantCardScroll
-          image="https://assets.website-files.com/60df9bbc248f30e7b966b47e/60df9ccce80d803f7fa78b2f_main_1.jpg"
-          flexRow="row"
-        />
-        <ContantCardScroll
-          image="https://assets.website-files.com/60df9bbc248f30e7b966b47e/60df9cad43b07d574230ba02_main_2-p-1080.jpeg"
-          flexRow="row-reverse"
-        />
-        <ContantCardScroll
-          image="https://assets.website-files.com/60df9bbc248f30e7b966b47e/60df9c7ede1b8cc0919a6325_main_3-p-1080.jpeg"
-          flexRow="row"
-        />
-        <ContantCardScroll
-          image="https://assets.website-files.com/60df9bbc248f30e7b966b47e/60df9c5aa411a46a6ebe21f3_main_4-p-1080.jpeg"
-          flexRow="row-reverse"
-        />
-        <ContantCardScroll
-          image="https://assets.website-files.com/60df9bbc248f30e7b966b47e/60df9c24787a006cce6d9458_main_5-p-1080.jpeg"
-          flexRow="row"
-        />
+        {scrollCardDB.map((card, i) => {
+          return (
+            <ContantCardScroll
+              key={i}
+              image={card.image}
+              flexRow={card.flexDir}
+            />
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/source/db/scrollCard.ts
@@ -0,0 +1,36 @@
+export const scrollCardDB: {
+  id: number;
+  image: string;
+  flexDir: 'row' | 'row-reverse';
+}[] = [
+  {
+    id: 1,
+    image:
+      'https://assets.website-files.com/60df9bbc248f30e7b966b47e/60df9ccce80d803f7fa78b2f_main_1.jpg',
+    flexDir: 'row',
+  },
+  {
+    id: 2,
+    image:
+      'https://assets.website-files.com/60df9bbc248f30e7b966b47e/60df9cad43b07d574230ba02_main_2-p-1080.jpeg',
+    flexDir: 'row-reverse',
+  },
+  {
+    id: 3,
+    image:
+      'https://assets.website-files.com/60df9bbc248f30e7b966b47e/60df9c7ede1b8cc0919a6325_main_3-p-1080.jpeg',
+    flexDir: 'row',
+  },
+  {
+    id: 4,
+    image:
+      'https://assets.website-files.com/60df9bbc248f30e7b966b47e/60df9c5aa411a46a6ebe21f3_main_4-p-1080.jpeg',
+    flexDir: 'row-reverse',
+  },
+  {
+    id: 5,
+    image:
+      'https://assets.website-files.com/60df9bbc248f30e7b966b47e/60df9c24787a006cce6d9458_main_5-p-1080.jpeg',
+    flexDir: 'row',
+  },
+];
